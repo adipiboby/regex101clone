8bd77e77127ade36fe9a5593d9dbad620cca2b2c
Adipi Manojkumar <adipimanojkumar@gmail.com>
2026-05-27 00:08:37 +0530

    removed clg

--- script.js
+++ script.js
@@ -80,5 +80,5 @@ contenteditable.addEventListener("input", (e) => {
     const test = regex.test(testusertext);
     // console.log(test);
   } 
-  // console.log(testusertext) 
+  console.log(testusertext) 
 }); 
